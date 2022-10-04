4c8fae1924401310f234ce0c26f7d0c27cc814ad
Kuba <kjuchnowski@gmail.com>
2022-10-04 15:02:01 +0200

    Added rsi calculator

--- listeners.py
+++ listeners.py
@@ -2,6 +2,7 @@ import requests
 import time
 
 from utils.constants import INTERVALS
+from utils.mathutils import calculate_price_difference
 
 
 # To make it a listener I will use it in while loop as separate thread.
@@ -38,3 +39,32 @@ def get_moving_average(symbol, interval, n):
         return average
     else:
         return 0
+
+
+def get_rsi(symbol, n, interval='1d'):
+    candles = get_candle_data(symbol, interval, n)
+    if len(candles) == n:
+        close_prices = [float(x[4]) for x in candles]
+        # close_prices[i] - close_prices[i-1] - Difference between prices
+        price_changes = calculate_price_difference(close_prices)
+        pos_price_changes = [p for p in price_changes if p > 0]
+        # print(pos_price_changes)
+        neg_price_changes = [p for p in price_changes if p < 0]
+        # print(neg_price_changes)
+        # RS = average gain / average loss
+        # Average gain = sum(pos_price_changes) / n
+        # Average loss = sum(neg_price_changes) / n -> Note we use absolute numbers
+        # RSI = 100 - 100 / (1 + RS)
+        avg_gain = sum(pos_price_changes) / (n - 1)
+        avg_loss = sum(neg_price_changes) / (n - 1)
+        rs = avg_gain / avg_loss
+        rsi = 100 - 100 / (1 + rs)
+        return rsi
+    else:
+        return 0
+
+
+# if __name__ == '__main__':
+#     x = get_rsi('BTCUSDT', 24, interval='15m')
+#     print(x)
+
--- utils/mathutils.py
+++ utils/mathutils.py
@@ -9,3 +9,6 @@ def calculate_quantity(current_price, amount_to_pay):
     return amount_to_pay / current_price
 
 
+def calculate_price_difference(prices: list):
+    differences = [j - i for i, j in zip(prices[:-1], prices[1:])]
+    return differences
