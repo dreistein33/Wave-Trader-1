f6306c7f722d6ee837a2785226dfe9063f2b3e3d
Kuba <kjuchnowski@gmail.com>
2022-10-04 13:27:49 +0200

    prevention

--- listeners.py
+++ listeners.py
@@ -31,8 +31,10 @@ def get_moving_average(symbol, interval, n):
     candles = get_candle_data(symbol, interval, n)
     # Use index 4 because Binance api provides data in lists
     # And the close price is actually placed at index four.
-    close_prices = [float(x[4]) for x in candles]
-    summed_prices = sum(close_prices)
-    average = summed_prices / len(close_prices)
-
+    if candles:
+        close_prices = [float(x[4]) for x in candles]
+        summed_prices = sum(close_prices)
+        average = summed_prices / len(close_prices)
+    else:
+        return
     return average
